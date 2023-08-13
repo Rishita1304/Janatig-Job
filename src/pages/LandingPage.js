c53557690b41f3bcd05aea129dc54363ba5b93d9
Rishita Srivastava <rixsri.13@gmail.com>
2023-08-13 17:21:44 +0530

    navbar changed

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,11 @@
 
+import Navbar from "../components/Navbar";
+
 const LandingPage = () => {
   return (
     <div className="relative bg-whitesmoke w-full h-[6324px] overflow-hidden text-left text-77xl text-black font-kamerik-105">
       <Navbar/>
+
     </div>
   );
 };
