339b82eab29ccb27bcf64f8f062f7646fa516731
Rishita Srivastava <rixsri.13@gmail.com>
2023-08-14 01:46:51 +0530

    Typing animation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,8 +9,8 @@ const Footer = () => {
         <div className="absolute top-[300px] left-[-300px] md:left-[46px] rounded-[12.66px] [background:linear-gradient(90.38deg,_#ffac75,_#fe6468_74.48%,_#ad71f2)] flex flex-row p-[20.2591495513916px] items-center justify-center text-[20.26px]">
           <b className="relative">Get Started</b>
         </div>
-        <div className="absolute top-[3.5px] left-[670px] box-border w-px h-[439px] border-r-[1px] border-solid border-dimgray-200" />
-        <div className="absolute top-[52px] left-[25px] md:left-[778px] flex flex-col items-start justify-start gap-[20px]">
+        <div className="absolute top-[3.5px] left-[640px] box-border w-px h-[439px] border-r-[1px] border-solid border-dimgray-200" />
+        <div className="absolute top-[52px] left-[25px] md:left-[728px] flex flex-col items-start justify-start gap-[20px]">
           <b className="relative">Menu</b>
           <div className="flex flex-col items-start justify-start gap-[18px] text-[20.26px] text-silver">
             <b className="relative">How it Works</b>
@@ -19,14 +19,14 @@ const Footer = () => {
           </div>
         </div>
         <img
-          className="absolute top-[270px] left-[20px] md:left-[763px] w-[119px] h-[116.3px] object-cover"
+          className="absolute top-[270px] left-[20px] md:left-[713px] w-[119px] h-[116.3px] object-cover"
           alt=""
           src="/image-1removebgpreview-2@2x.png"
         />
-        <b className="absolute top-[329px] left-[130px] md:left-[882px]">X.com</b>
+        <b className="absolute top-[329px] left-[130px] md:left-[822px]">X.com</b>
       </div>
       <div className="absolute top-[5246px] left-[calc(50%_-_220px)] md:left-[0px] bg-gray w-[450px] md:w-[1440px] h-[58px] overflow-hidden text-xl text-dimgray-100">
-        <b className="absolute top-[calc(50%_-_12px)] left-[calc(50%_-_119px)]">
+        <b className="absolute top-[calc(50%_-_12px)] left-[calc(50%_-_170px)]">
           Made with 🧡 by x.com
         </b>
       </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,22 @@
+import { useTypewriter } from "react-simple-typewriter";
+
 const Hero = () => {
+  const [text]= useTypewriter({
+    words: ['Outreach','Marketing'],
+    loop: {},
+    typeSpeed: 10,
+    deleteSpeed: 50
+  });
   return (
     <div className="absolute top-[200px] md:left-[205px] left-[10px] w-[500px] md:w-[890px] h-[252px] text-5xl md:text-45xl text-darkslategray">
 
       <div className="absolute top-[0px] left-[0px] md:w-[791px] w-[300px] h-[252px]">
 
-        <b className="absolute top-[0px] left-[0px]">We’d love to help</b>
+        <b className="absolute top-[0px] font-kamerik-105 left-[0px]">We’d love to help</b>
 
         <b className="absolute top-[40px] md:top-[88px] left-[calc(50%_-_25%)] md:left-[250px]">Solve Your</b>
 
-        <b className="absolute top-[80px] left-[calc(50%_-_35%)] md:top-[176px] md:left-[110px]"> Outreach Problems</b>
+        <b className="absolute top-[80px] left-[calc(50%_-_35%)] md:top-[176px] md:left-[110px]"> <span> {text}</span> Problems</b>
 
       </div>
 
