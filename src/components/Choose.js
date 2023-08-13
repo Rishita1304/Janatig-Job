be10e4ecdfec8243aefd279af35f7ce75dcc5437
Rishita Srivastava <rixsri.13@gmail.com>
2023-08-14 00:37:38 +0530

    choose section responsive

--- src/components/Choose.js
+++ src/components/Choose.js
@@ -1,7 +1,7 @@
 const Choose = () => {
   return (
     <>
-      <div className="absolute top-[2200px] md:top-[3176px] left-[10px] md:left-[49px] flex flex-row items-start justify-start gap-[10px]">
+      <div className="absolute top-[2200px] md:top-[3176px] left-[calc(50%_-_175px)] md:left-[49px] flex flex-row items-start justify-start gap-[10px]">
         <div className="relative rounded-11xl box-border w-[350px] md:w-[380px] h-[400px] overflow-hidden shrink-0 z-[0] border-[1px] border-solid border-black" />
         <div className="my-0 mx-[!important] absolute top-[calc(50%_-_187px)] left-[20px] md:left-[calc(50%_-_177.5px)] flex flex-col items-start justify-start gap-[114px] z-[1]">
           <b className="relative">🙅</b>
@@ -13,7 +13,7 @@ const Choose = () => {
           </div>
         </div>
       </div>
-      <div className="absolute top-[3800px] md:top-[3976px] left-[10px] md:left-[809px] flex flex-row items-start justify-start gap-[10px]">
+      <div className="absolute top-[3800px] md:top-[3976px] left-[calc(50%_-_175px)] md:left-[809px] flex flex-row items-start justify-start gap-[10px]">
         <div className="relative rounded-11xl box-border w-[350px] md:w-[380px] h-[400px] overflow-hidden shrink-0 z-[0] border-[1px] border-solid border-black" />
         <div className="my-0 mx-[!important] absolute top-[calc(50%_-_201px)] left-[20px] md:left-[calc(50%_-_177.5px)] flex flex-col items-start justify-start gap-[114px] z-[1]">
           <b className="relative">🏆</b>
@@ -26,7 +26,7 @@ const Choose = () => {
           </div>
         </div>
       </div>
-      <div className="absolute top-[2600px] md:top-[3576px] left-[10px] md:left-[49px] flex flex-row items-start justify-start gap-[10px]">
+      <div className="absolute top-[2600px] md:top-[3576px] left-[calc(50%_-_175px)] md:left-[49px] flex flex-row items-start justify-start gap-[10px]">
         <div className="relative rounded-11xl box-border w-[350px] md:w-[380px] h-[400px] overflow-hidden shrink-0 z-[0] border-[1px] border-solid border-black" />
         <div className="my-0 mx-[!important] absolute top-[calc(50%_-_187px)] left-[20px] md:left-[calc(50%_-_177.5px)] flex flex-col items-start justify-start gap-[114px] z-[1]">
           <b className="relative">🏎️</b>
@@ -38,7 +38,7 @@ const Choose = () => {
           </div>
         </div>
       </div>
-      <div className="absolute top-[3000px] md:top-[3576px] left-[10px] md:left-[429px] flex flex-row items-start justify-start gap-[10px]">
+      <div className="absolute top-[3000px] md:top-[3576px] left-[calc(50%_-_175px)] md:left-[429px] flex flex-row items-start justify-start gap-[10px]">
         <div className="relative rounded-11xl box-border w-[350px] md:w-[380px] h-[400px] overflow-hidden shrink-0 z-[0] border-[1px] border-solid border-black" />
         <div className="my-0 mx-[!important] absolute top-[calc(50%_-_215px)] left-[20px] md:left-[calc(50%_-_177.5px)] flex flex-col items-start justify-start gap-[114px] z-[1]">
           <b className="relative">🎯</b>
@@ -51,7 +51,7 @@ const Choose = () => {
           </div>
         </div>
       </div>
-      <div className="absolute top-[3400px] md:top-[3576px] left-[10px] md:left-[809px] flex flex-row items-start justify-start gap-[10px]">
+      <div className="absolute top-[3400px] md:top-[3576px] left-[calc(50%_-_175px)] md:left-[809px] flex flex-row items-start justify-start gap-[10px]">
         <div className="relative rounded-11xl box-border w-[350px] md:w-[380px] h-[400px] overflow-hidden shrink-0 z-[0] border-[1px] border-solid border-black" />
         <div className="my-0 mx-[!important] absolute top-[calc(50%_-_201px)] left-[20px] md:left-[calc(50%_-_177.5px)] flex flex-col items-start justify-start gap-[114px] z-[1]">
           <b className="relative">🛠</b>
@@ -67,14 +67,14 @@ const Choose = () => {
         <p className="m-0">Why choose us?</p>
         <p className="m-0">what makes us different?</p>
       </div>
-      <b className="absolute top-[4828px] left-[calc(50%_-_334px)] text-45xl text-darkslategray">
+      <b className="absolute top-[4300px] md:top-[4478px] left-[calc(50%_-_175px)] md:left-[calc(50%_-_304px)] text-21xl md:text-45xl text-darkslategray">
         Still not convinced?
       </b>
-      <div className="absolute top-[4915px] left-[396px] text-xl text-dimgray-300 text-center flex items-center justify-center w-[657px]">
+      <div className="absolute top-[4400px] md:top-[4570px] left-[calc(50%_-_175px)] md:left-[calc(50%_-_328px)] text-xl text-dimgray-300 text-center flex items-center justify-center w-[350px] md:w-[600px]">
         Schedule a call to learn more about how we can change the way you view
         sourcing your ads...forever.
       </div>
-      <div className="absolute top-[5005px] left-[592px] rounded-[16.27px] [background:linear-gradient(90.38deg,_#ffac75,_#fe6468_74.48%,_#ad71f2)] flex flex-row p-[26.039215087890625px] items-center justify-center text-[26.04px] text-white">
+      <div className="absolute top-[4500px] md:top-[4650px] left-[calc(50%_-_110px)] md:left-[calc(50%_-_135px)] cursor-pointer rounded-[16.27px] [background:linear-gradient(90.38deg,_#ffac75,_#fe6468_74.48%,_#ad71f2)] flex flex-row p-[19px] md:p-[26px] items-center justify-center text-[26px] text-white">
         <b className="relative">schedule a call</b>
       </div>
     </>
