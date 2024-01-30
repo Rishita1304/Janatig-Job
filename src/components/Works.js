c29ac172973ea4a682db445d41ef0816f305d032
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-01-30 11:40:40 +0530

    works changed

--- src/components/Works.js
+++ src/components/Works.js
@@ -3,7 +3,7 @@ const Works = () => {
     <>
       <b className="absolute font-kamerik-105 top-[1220px] md:top-[1470px] left-[20px] md:left-[71px] text-[27px] md:text-[52px] flex text-darkslategray items-center w-[370px] md:w-[750px]">
         “The real fact of the matter is that nobody reads ads. People read what
-        interests them,  it’s an ad”
+        ,  it’s an ad”
       </b>
       <b className="absolute top-[1450px] md:top-[1880px] text-[25px] md:text-[72px] left-[5px] md:left-[80px] text-darkslategray">
         how it works
